90993753213ea9b30c842e2ea43aca64395d1705
Cyril1355 <bcyrildu13@gmail.com>
2026-06-19 13:37:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -124,3 +124,9 @@ document.addEventListener('pointerdown', function(event) {
         }
     }
 });
+// Injection automatique du favicon sur toutes les pages
+const favicon = document.createElement('link');
+favicon.rel = 'icon';
+favicon.type = 'image/jpeg';
+favicon.href = 'https://cyril1355.github.io/Cyril-freelance/favicon.jpg'; 
+document.head.appendChild(favicon);
