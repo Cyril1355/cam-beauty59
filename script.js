e461bb1c7539f942b9534c43685ab325426fc13c
Cyril1355 <bcyrildu13@gmail.com>
2026-07-27 19:03:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -37,18 +37,19 @@ function initHeader() {
 
         const href = link.getAttribute('href');
 
-if (
-    href !== "#" &&
-    path.endsWith(href) &&
-    !link.classList.contains("btn-primary") &&
-    !link.classList.contains("btn-cta")
-) {
-    link.classList.add("active");
-}
+        if (
+            href !== "#" &&
+            path.endsWith(href) &&
+            !link.classList.contains("btn-primary") &&
+            !link.classList.contains("btn-cta")
+        ) {
+            link.classList.add("active");
 
             const parentDropdown = link.closest('.dropdown');
             if (parentDropdown) {
-                parentDropdown.querySelector('.dropdown-trigger').classList.add('active');
+                parentDropdown
+                    .querySelector('.dropdown-trigger')
+                    .classList.add('active');
             }
         }
     });
