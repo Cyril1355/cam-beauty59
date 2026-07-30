a9d74b3e798ab53c3217cb557b640f0720c813f9
Cyril1355 <bcyrildu13@gmail.com>
2026-07-30 19:09:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -434,7 +434,7 @@ function updateBookingSelection() {
         if (hasRemplissage1) {
             targetUrl = "https://tidycal.com/camillebrejnakowski/cil-a-cil-remplissage-3-semaines";
         }
-      else if (hasRemplissage2) {
+        else if (hasRemplissage2) {
             targetUrl = "https://tidycal.com/camillebrejnakowski/cil-a-cil-remplissage1-3-semaines";
         }
     }
@@ -445,7 +445,7 @@ function updateBookingSelection() {
         if (hasRemplissage1) {
             targetUrl = "https://tidycal.com/camillebrejnakowski/mixte-remplissage-3-semaines";
         }
-      else if (hasRemplissage2) {
+        else if (hasRemplissage2) {
             targetUrl = "https://tidycal.com/camillebrejnakowski/mixte-remplissage1-3-semaines";
         }
     }
